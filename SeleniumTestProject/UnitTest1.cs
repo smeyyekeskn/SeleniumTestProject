c24e70beb90a71c1c35c4832c5cfa6de23aa6b41
smeyyekeskn <smeyyekeskn@gmail.com>
2019-04-16 13:02:01 +0300

    Proje dosyası ekle.

---
+++ SeleniumTestProject/UnitTest1.cs
@@ -0,0 +1,39 @@
+using NUnit.Framework;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Remote;
+using System;
+using System.IO;
+
+namespace Tests
+{
+    [TestFixture]
+    public class Tests
+    {
+
+
+        private RemoteWebDriver driver;
+
+        [SetUp]
+        public void Setup()
+        {
+            string driverDirectory = Path.Combine(Path.GetDirectoryName(Path.GetDirectoryName(Path.GetDirectoryName(Environment.CurrentDirectory))), "Driver");
+            driver = new ChromeDriver(driverDirectory);
+            driver.Manage().Window.Maximize();
+            driver.Navigate().GoToUrl("http://www.n11.com");
+        }
+
+        [TearDown]
+        public void Close()
+        {
+            driver.Quit();
+        }
+
+        [Test]
+        public void TestSiteTitle()
+        {
+            var title = driver.Title;
+
+            Assert.IsTrue(title == "n11.com - Alýþveriþin Uðurlu Adresi");
+        }
+    }
+}
